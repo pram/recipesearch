868ded5779565dd2bef6915e6ad35ba46caf09d0
pram <pram@attale.com>
2015-09-02 19:42:04 +0100

    adding templateurl

--- app/scripts/directives/data.js
+++ app/scripts/directives/data.js
@@ -10,14 +10,6 @@ angular.module('estestApp')
   .directive('data', function () {
     return {
         restrict: 'E', //E = element, A = attribute, C = class, M = comment         
-        //scope: {
-            //@ reads the attribute value, = provides two-way binding, & works with functions
-        //    title: '@'         },
-        //template: '<div>{{ myVal }}</div>',
         templateUrl: 'templates/html-includes/angular-chart-template.html',
-        //controller: controllerFunction, //Embed a custom controller in the directive
-        link: function ($scope, element) {
-        	element.text('Boooooooo!!');
-        } //DOM manipulation
     };
   });
