778f8ec4652a138def21b96636fd81c0fad15d3f
pram <pram@attale.com>
2015-10-21 17:35:04 +0100

    adding file downloader

---
+++ loader/download_file.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download('http://openrecipes.s3.amazonaws.com/recipeitems-latest.json.gz', 'recipeitems-latest.json.gz', function(){
+  console.log('done');
+});
