e2710c81ad987317a4a984365f4f5aee7a70b549
pram <pram@attale.com>
2015-10-21 18:50:19 +0100

    updating header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -15,12 +15,12 @@ class Header extends Component {
         <div className="Header-container">
           <a className="Header-brand" href="/" onClick={Link.handleClick}>
             <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
+            <span className="Header-brandTxt">Naughty Zombie</span>
           </a>
           <Navigation className="Header-nav" />
           <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
+            <h1 className="Header-bannerTitle">Recipe Search</h1>
+            <p className="Header-bannerDesc">Searching for Recipes</p>
           </div>
         </div>
       </div>
