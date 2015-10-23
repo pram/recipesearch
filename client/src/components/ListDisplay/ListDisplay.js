794a094c8673035f734b86b0b5f09afe752e8071
pram <pram@attale.com>
2015-10-23 19:47:31 +0100

    custom display widget working!

--- client/src/components/ListDisplay/ListDisplay.js
+++ client/src/components/ListDisplay/ListDisplay.js
@@ -3,13 +3,105 @@ import withStyles from '../../decorators/withStyles';
 import styles from './ListDisplay.css';
 
 @withStyles(styles)
+class ProductCategoryRow extends Component {
+  render() {
+    return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+  }
+}
+
+export default ProductCategoryRow;
+
+class ProductRow extends Component {
+  render() {
+    var name = this.props.product.stocked ?
+      this.props.product.name :
+      <span style={{color: 'red'}}>
+                {this.props.product.name}
+            </span>;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+  }
+}
+
+export default ProductRow;
+
+class ProductTable extends Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach(function(product) {
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+export default ProductTable;
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+
+class FilterableProductTable extends Component{
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+};
+
+export default FilterableProductTable;
+
+var PRODUCTS = [
+  {category: 'Cheeese', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Chicken', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
+];
+
+@withStyles(styles)
 class ListDisplay extends Component {
   render() {
     return (
-      <div className="ListDisplay">
-        <p className="ListDisplay-input">This is the Control</p>
-      </div>
+      <FilterableProductTable products={PRODUCTS} />
     );
+    document.getElementById('container');
   }
 }
 export default ListDisplay;
