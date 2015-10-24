b9a3838bd4a762ba89ce05d685bc21b93ec58b9e
Pram Attale <pram@attale.com>
2015-10-24 08:23:18 -0700

    small update

--- client/src/components/ListDisplay/ListDisplay.js
+++ client/src/components/ListDisplay/ListDisplay.js
@@ -92,7 +92,7 @@ var PRODUCTS = [
   {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
   {category: 'Chicken', price: '$99.99', stocked: true, name: 'iPod Touch'},
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
+  {category: 'Big Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
 ];
 
 @withStyles(styles)
